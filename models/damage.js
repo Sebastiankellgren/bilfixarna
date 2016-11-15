c0f5971413ee6c2645ec2e6926ca55b33cb9d9f8
Sebastian Kellgren <Sebastian Kellgren>
2016-11-15 12:25:19 +0100

    damage model

--- models/damage.js
+++ models/damage.js
@@ -1,8 +1,8 @@
 // Create a new mongoose schema
 var Schema = m.mongoose.Schema({
   description: {type: String, required: true},
-  parts: {type: Schema.ObjectId, ref: 'Spare-Parts', required: true},
-  workers: {type: Schema.ObjectId, ref: 'Employees', required: true},
+  parts: {type: Schema.Types.ObjectId, ref: 'Spare_Parts', required: true},
+  workers: {type: Schema.Types.ObjectId, ref: 'Employees', required: true},
   hours: {type: Number, required: true},
   status: {type: String, required: true}
 });
