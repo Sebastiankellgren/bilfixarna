6f2b257c474816e55658d003ae71bac787cfe5b3
Sebastian Kellgren <Sebastian Kellgren>
2016-11-18 15:47:32 +0100

    address

--- models/customers.js
+++ models/customers.js
@@ -3,7 +3,7 @@ var Schema = m.mongoose.Schema({
   SSN: {type: Number, required: true},
   fName: {type: String, required: true},
   lName: {type: String, required: true},
-  address: {type: Object, required: true},
+  address: {type: String, required: true},
   email: {type: String, required: true}
 });
 
